7228eb87f8464dbda1b0f913f36baf8170e566f9
cstone <cstone@pobox.com>
2019-04-29 22:04:07 -0700

    more

--- sersnoop.py
+++ sersnoop.py
@@ -5,6 +5,8 @@ import serial
 import time
 import select
 
+BAUDRATE = 4800
+
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         print("args: devA devB")
@@ -12,6 +14,7 @@ if __name__ == '__main__':
     devA, devB = sys.argv[1], sys.argv[2]
     sera = serial.Serial()
     sera.port = devA
+    sera.baudrate = BAUDRATE
     sera.parity = serial.PARITY_NONE
     sera.bytesize = serial.EIGHTBITS
     sera.rtscts = False
@@ -22,6 +25,7 @@ if __name__ == '__main__':
     sys.stderr.write(f'A ({devA}) open\n')
     serb = serial.Serial()
     serb.port = devB
+    serb.baudrate = BAUDRATE
     serb.parity = serial.PARITY_NONE
     serb.bytesize = serial.EIGHTBITS
     serb.rtscts = False
@@ -37,15 +41,16 @@ if __name__ == '__main__':
         ready, _, _ = select.select([sera,serb], [], [])
         lastts = curts
         curts = time.time() * 1000
+        diff = curts-lastts
         for r in ready:
             if r == sera:
                 c = sera.read(1)
                 serb.write(c)
-                print(f'{"%.5f" % curts} A: {"%02x" % c[0]}')
+                print(f'{"%.5f" % curts} {"%2.4f" % diff} A: {"%02x" % c[0]}')
             elif r == serb:
                 c = serb.read(1)
                 sera.write(c)
-                print(f'{"%.5f" % curts} B: {"%02x" % c[0]}')
+                print(f'{"%.5f" % curts} {"%2.4f" % diff} B: {"%02x" % c[0]}')
 
 
 
